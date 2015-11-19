0785f67978152a07ce8e79b6ace0aac6a8c553c6
Jared Ririe <jaredririe20@gmail.com>
2015-11-19 20:13:00 +0000

    [Front end] Render basic components using React, some navigation

---
+++ app/js/app.js
@@ -0,0 +1,128 @@
+var Router = ReactRouter.Router;
+var Link = ReactRouter.Link;
+var Route = ReactRouter.Route;
+var DefaultRoute = Router.DefaultRoute;
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="container">
+              <div className="navbar-header">
+                 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                   <span className="sr-only">Toggle navigation</span>
+                   <span className="icon-bar"></span>
+                   <span className="icon-bar"></span>
+                   <span className="icon-bar"></span>
+                </button>
+                <Link className="navbar-brand" to="home">Beginning</Link>
+              </div>
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                  <li><Link to="page">Page</Link></li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+        <div className="container">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>Put your home page here</p>
+      </div>
+    );
+  }
+});
+
+var Page = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Page</h1>
+        <p>Demo another page here</p>
+        {element}
+      </div>
+    );
+  }
+});
+
+////////////////////////////////////
+
+var Badge = React.createClass({
+  render: function() {
+    return <button className="btn btn-primary" type="button">
+      {this.props.title} <span className="badge">{this.props.number}</span>
+    </button>
+  }
+});
+
+var Thumbnail = React.createClass({
+  render: function() {
+    return <div className="col-sm-6 col-md-4">
+      <div className="thumbnail">
+        <img src={this.props.imageUrl} alt="..."></img>
+        <div className="caption">
+          <h3>{this.props.header}</h3>
+          <p>{this.props.description}</p>
+          <p>
+            <Badge title={this.props.title} number={this.props.number} />
+          </p>
+        </div>
+      </div>
+    </div>
+  }
+});
+
+var ThumbnailList = React.createClass({
+  render: function() {
+    var list = this.props.thumbnailData.map(function(thumbnailProps){
+      return <Thumbnail {...thumbnailProps} />
+    });
+
+    return <div>
+      {list}
+    </div>
+  }
+});
+
+var options = {
+  thumbnailData:  [{
+    title: 'Show Courses',
+    number: 12,
+    header: 'Learn React',
+    description: 'React is a fantastic new front end library for rendering web pages. React is a fantastic new front end library for rendering web pages.',
+    imageUrl: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png'
+  },{
+    title: 'Show Courses',
+    number: 25,
+    header: 'Learn Gulp',
+    description: 'Gulp will speed up your development workflow.  Gulp will speed up your development workflow.  Gulp will speed up your development workflow.',
+    imageUrl: 'http://brunch.io/images/others/gulp.png'
+  }]
+};
+
+var element = React.createElement(ThumbnailList, options);
+
+//////////////////////////
+
+// Run the routes
+var routes = (
+      <Router>
+        <Route name="app" path="/" component={App}>
+          <Route name="page" path="/page" component={Page} />
+          <Route name="home" path="/home" component={Home} />
+        </Route>
+      </Router>
+);
+
+ReactDOM.render(routes, document.body);
