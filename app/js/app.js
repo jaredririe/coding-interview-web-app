0a2f4c5eeb7d2a8a61ccd0a89adde7e043bd8b48
Jared Ririe <jaredririe20@gmail.com>
2015-11-20 16:56:39 +0000

    [Front end] Started creating home page with questions, fixed routing, attempted installation of webpack

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,8 @@
 var Router = ReactRouter.Router;
 var Link = ReactRouter.Link;
 var Route = ReactRouter.Route;
-var DefaultRoute = Router.DefaultRoute;
+var IndexRoute = ReactRouter.IndexRoute;
+// var Home = require('./home.js');
 
 var App = React.createClass({
   render: function() {
@@ -10,13 +11,16 @@ var App = React.createClass({
         <nav className="navbar navbar-default" role="navigation">
           <div className="container">
               <div className="navbar-header">
-                 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                 <button type="button"
+                         className="navbar-toggle"
+                         data-toggle="collapse"
+                         data-target="#bs-example-navbar-collapse-1">
                    <span className="sr-only">Toggle navigation</span>
                    <span className="icon-bar"></span>
                    <span className="icon-bar"></span>
                    <span className="icon-bar"></span>
                 </button>
-                <Link className="navbar-brand" to="home">Beginning</Link>
+                <Link className="navbar-brand" to="/">Beginning</Link>
               </div>
               <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul className="nav navbar-nav">
@@ -33,23 +37,23 @@ var App = React.createClass({
   }
 });
 
-var Home = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <h1>Home</h1>
-        <p>Put your home page here</p>
-      </div>
-    );
-  }
-});
-
 var Page = React.createClass({
   render: function() {
     return (
       <div>
         <h1>Page</h1>
         <p>Demo another page here</p>
+      </div>
+    );
+  }
+});
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>Put your home page here</p>
         {element}
       </div>
     );
@@ -60,54 +64,62 @@ var Page = React.createClass({
 
 var Badge = React.createClass({
   render: function() {
-    return <button className="btn btn-primary" type="button">
-      {this.props.title} <span className="badge">{this.props.number}</span>
-    </button>
+    return (
+      <button className="btn btn-primary" type="button">
+        {this.props.title} <span className="badge">{this.props.number}</span>
+      </button>
+    );
   }
 });
 
 var Thumbnail = React.createClass({
   render: function() {
-    return <div className="col-sm-6 col-md-4">
-      <div className="thumbnail">
-        <img src={this.props.imageUrl} alt="..."></img>
-        <div className="caption">
-          <h3>{this.props.header}</h3>
-          <p>{this.props.description}</p>
-          <p>
-            <Badge title={this.props.title} number={this.props.number} />
-          </p>
+    return (
+      <div className="col-sm-6 col-md-4">
+        <div className="thumbnail">
+          <div className="caption">
+            <h3>{this.props.header}</h3>
+            <p>{this.props.description}</p>
+            <p>
+              <Badge title={this.props.title} number={this.props.number} />
+              {this.props.timestamp}-{this.props.username}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    );
   }
 });
 
 var ThumbnailList = React.createClass({
   render: function() {
     var list = this.props.thumbnailData.map(function(thumbnailProps){
-      return <Thumbnail {...thumbnailProps} />
+      return (<Thumbnail {...thumbnailProps} />);
     });
 
-    return <div>
-      {list}
-    </div>
+    return (
+      <div>
+        {list}
+      </div>
+    );
   }
 });
 
 var options = {
   thumbnailData:  [{
-    title: 'Show Courses',
-    number: 12,
-    header: 'Learn React',
-    description: 'React is a fantastic new front end library for rendering web pages. React is a fantastic new front end library for rendering web pages.',
-    imageUrl: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png'
+    title: 'Answer Question',
+    header: 'Question Title 1',
+    description: 'Question Body',
+    timestamp: 'time',
+    username: 'username',
+    // imageUrl: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png'
   },{
-    title: 'Show Courses',
-    number: 25,
-    header: 'Learn Gulp',
-    description: 'Gulp will speed up your development workflow.  Gulp will speed up your development workflow.  Gulp will speed up your development workflow.',
-    imageUrl: 'http://brunch.io/images/others/gulp.png'
+    title: 'Answer Question',
+    header: 'Question Title 1',
+    description: 'Question Body',
+    timestamp: 'time',
+    username: 'username',
+    // imageUrl: 'http://brunch.io/images/others/gulp.png'
   }]
 };
 
@@ -119,8 +131,8 @@ var element = React.createElement(ThumbnailList, options);
 var routes = (
       <Router>
         <Route name="app" path="/" component={App}>
+          <IndexRoute component={Home}/>
           <Route name="page" path="/page" component={Page} />
-          <Route name="home" path="/home" component={Home} />
         </Route>
       </Router>
 );
