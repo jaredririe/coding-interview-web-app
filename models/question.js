ef5953e52137d985f7c5de40d49a9bcdd9e1ba41
Jared Ririe <jaredririe20@gmail.com>
2015-12-10 19:05:11 +0000

    Started working on questionsSchema. Removed node_modules folder from github.

---
+++ models/question.js
@@ -0,0 +1,29 @@
+// setup Mongoose
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var findOrCreate = require('mongoose-findorcreate')
+
+var User = require('./user.js');
+
+// Item schema
+var questionSchema = new Schema({
+  user: {type: ObjectId, ref: 'users'},
+  header: String,
+  body: String,
+  timestamp: {type: Date, default: Date.now},
+  votes: {type: Number, default: 0}
+});
+
+// ensure schemas use virtual IDs
+questionSchema.set('toJSON', {
+  virtuals: true
+});
+
+// add findorCreate
+questionSchema.plugin(findOrCreate);
+
+// create item
+var Question = mongoose.model('questions', questionSchema);
+
+module.exports = Question;
