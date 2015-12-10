287e57e4c947e00665bf5936b7f8b7eb32f3abc7
Peter Zabriskie <pete.zabriskie@gmail.com>
2015-12-10 12:01:44 -0700

    Added start script and changed port

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ var User = require('./models/user.js');
 var Question = require('./models/question.js');
 var Answer = require('./models/answer.js');
 
+app.set('port', (process.env.PORT || 5000));
 // start the server
-var server = app.listen(3000, function() {
-  console.log("Started on port " + 3000);
+var server = app.listen(app.get('port'), function() {
+  console.log("Started on port " + app.get('port'));
   //var host = server.address().address;
   //var port = server.address().port;
   var host = server.address().address;
-  var port = 3000;
+  var port = app.get('port');
 });
